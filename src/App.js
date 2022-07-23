e470c9865482534796b6ffb9d19d8f7b493dbd1f
Chris Neal <chrisvneal@gmail.com>
2022-07-23 01:03:45 -1000

    Create song input and song list sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./styles.scss";
 
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <section className="song-input--section">
+        <h2>Song Input</h2>
+        Song Title: <input />
+        Song Genre: <input />
+      </section>
+      <section className="song-lists">
+        <ul className="song-list">
+          <li className="song-item">
+            <span className="song-title">The Time Has Come</span>
+            <span className="song-author">Chris Rockman</span>
+
+            <span className="song-genre">R &amp; B</span>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
