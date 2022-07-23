88c4002ea56de1c3c08530083ea21a906a5a8f6e
Chris Neal <chrisvneal@gmail.com>
2022-07-23 02:14:50 -1000

    Changed "genre" input to dropdown menu

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ export default function App() {
         <form>
           <h2>Song Input</h2>
           Song Title: <input type="text" />
-          Song Genre: <input type="text" />
+          Song Genre:{" "}
+          <select>
+            <option value="hiphop">Hip Hop</option>
+            <option value="rnb">R &amp; B</option>
+          </select>
           <button>Submit Song</button>
         </form>
       </section>
