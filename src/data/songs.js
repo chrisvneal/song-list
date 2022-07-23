af21363ce66a7e7d59114bafa911fe71402ea6da
Chris Neal <chrisvneal@gmail.com>
2022-07-23 02:59:55 -1000

    Import song data from data folder

---
+++ src/data/songs.js
@@ -0,0 +1,37 @@
+export const songs = [
+  {
+    title: "The Time Has Come",
+    author: "Chris Rockman",
+    genre: "Hip-Hop"
+  },
+  {
+    title: "Who Ever Thought",
+    author: "DJ Tumor",
+    genre: "Hip-Hop"
+  },
+  {
+    title: "Old School Fashion",
+    author: "Mr. Murderous",
+    genre: "Hip-Hop"
+  },
+  {
+    title: "Please Come Back",
+    author: "City Girls",
+    genre: "R & B"
+  },
+  {
+    title: "My Bitch Be Trippin",
+    author: "Tony Saliva",
+    genre: "R & B"
+  },
+  {
+    title: "Pu**y like Peanut Butter",
+    author: "Land of Ken",
+    genre: "R & B"
+  },
+  {
+    title: "Big Titties",
+    author: "Pretty TyReeq",
+    genre: "R & B"
+  }
+];
