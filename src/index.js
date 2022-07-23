abcdbbf8216f2bdb6a563d0ad27574a0ce59c2bc
Chris Neal <chrisvneal@gmail.com>
2022-07-23 03:27:47 -1000

    Initiate state through reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import { createRoot } from "react-dom/client";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+
 import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const store = createStore(reducers);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return "Thank you";
+};
+
+export default combineReducers({ songs: songsReducer });
