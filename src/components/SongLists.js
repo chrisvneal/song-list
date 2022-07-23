785726ca0b69aa206309a4d104bdc505b584e124
Chris Neal <chrisvneal@gmail.com>
2022-07-23 03:03:10 -1000

    Change text 'author' to 'artist' in code

--- src/components/SongLists.js
+++ src/components/SongLists.js
@@ -11,17 +11,17 @@ const SongLists = () => {
         <ul>
           <li className="song-item">
             <span className="song-title">The Time Has Come</span>
-            <span className="song-author">Chris Rockman</span>
+            <span className="song-artist">Chris Rockman</span>
             <span className="song-genre">Hip-Hop</span>
           </li>
           <li className="song-item">
             <span className="song-title">Who Ever Thought</span>
-            <span className="song-author">DJ Tumor</span>
+            <span className="song-artist">DJ Tumor</span>
             <span className="song-genre">Hip-Hop</span>
           </li>
           <li className="song-item">
             <span className="song-title">Old School Fashion</span>
-            <span className="song-author">Mr. Murderous</span>
+            <span className="song-artist">Mr. Murderous</span>
             <span className="song-genre">Hip-Hop</span>
           </li>
         </ul>
@@ -33,22 +33,22 @@ const SongLists = () => {
         <ul>
           <li className="song-item">
             <span className="song-title">Please Come Back</span>
-            <span className="song-author">City Girls</span>
+            <span className="song-artist">City Girls</span>
             <span className="song-genre">R &amp; B</span>
           </li>
           <li className="song-item">
             <span className="song-title">My Bitch Be Trippin</span>
-            <span className="song-author">Tony Saliva</span>
+            <span className="song-artist">Tony Saliva</span>
             <span className="song-genre">R &amp; B</span>
           </li>
           <li className="song-item">
             <span className="song-title">Pu**y like Peanut Butter</span>
-            <span className="song-author">Land of Ken</span>
+            <span className="song-artist">Land of Ken</span>
             <span className="song-genre">R &amp; B</span>
           </li>
           <li className="song-item">
             <span className="song-title">Smoove City</span>
-            <span className="song-author">Pretty Tyson</span>
+            <span className="song-artist">Pretty Tyson</span>
             <span className="song-genre">R &amp; B</span>
           </li>
         </ul>
