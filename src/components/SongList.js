95140a19fbe0c288de9d66aaf627bc821b42ebf7
Chris Neal <chrisvneal@gmail.com>
2022-07-24 04:27:46 -1000

    Create song count variable for SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,10 +3,12 @@ import { connect } from "react-redux";
 
 import SongItem from "./SongItem";
 
-const SongList = ({ genre, songs }) => {
+const SongList = ({ genre, songs, songCount }) => {
   return (
     <div className="song-list">
-      <h3 className="song-genre">{genre}</h3>
+      <h3 className="song-genre">
+        {genre} <span className="song-count">{songCount} songs</span>
+      </h3>
       <ul>
         {songs.map((song) => (
           <SongItem song={song} key={song.title} />
@@ -19,7 +21,7 @@ const SongList = ({ genre, songs }) => {
 const mapStateToProps = (state, ownProps) => {
   // Filter songs based on their genre, provided by SongList props
   const songs = state.songs.filter((song) => song.genre === ownProps.genre);
-  return { songs };
+  return { songs, songCount: songs.length };
 };
 
 export default connect(mapStateToProps)(SongList);
