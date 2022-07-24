6fd46450953d9bd190b1d9cb6327055f25d341e9
Chris Neal <chrisvneal@gmail.com>
2022-07-24 04:12:33 -1000

    Add lodash dependency to select unique music genres from state

--- src/components/SongLists.js
+++ src/components/SongLists.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import SongList from "./SongList";
 
-const SongLists = () => {
-  const [genres] = useState(["Hip-Hop", "R & B", "Trap"]);
+const SongLists = ({ genres }) => {
+  // const [genres] = useState(["Hip-Hop", "R & B", "Trap"]);
   const renderLists = genres.map((genre) => (
     <SongList genre={genre} key={genre} />
   ));
 
-  return <div className="song-lists">{renderLists}</div>;
+  return (
+    <div className="song-lists">
+      <h2>Song Lists</h2>
+      {renderLists}
+    </div>
+  );
 };
 
 export default SongLists;
